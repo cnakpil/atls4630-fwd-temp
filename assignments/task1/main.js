2d7fab03258d46aabc16b0549697505c839aa56e
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-09-13 09:25:24 -0600

    update main.js

--- assignments/task1/main.js
+++ assignments/task1/main.js
@@ -1,3 +1,24 @@
+const ttt = {
+    xTurn: true,
+    xState: [],
+    oState: [],
+    winStates: [
+        // Rows
+        ['0', '1', '2'],
+        ['3', '4', '5'],
+        ['6', '7', '8'],
+
+        // Columns
+        ['0', '3', '6'],
+        ['1', '4', '7'],
+        ['2', '5', '8'],
+
+        // Diagonal
+        ['0', '4', '8'],
+        ['2', '4', '6']
+    ]
+}
+
 document.addEventListener('click', event => {
     const target = event.target
     const isCell = target.classList.contains('cell')
@@ -6,13 +27,13 @@ document.addEventListener('click', event => {
     if (isCell && !isDisabled) {
         const cellValue = target.dataset.value
 
-        board.xTurn === true
-            ? board.xState.push(cellValue)
-            : board.oState.push(cellValue)
+        ttt.xTurn === true
+            ? ttt.xState.push(cellValue)
+            : ttt.oState.push(cellValue)
 
         target.classList.add('disabled')
-        target.classList.add(board.xTurn ? 'x' : 'o')
+        target.classList.add(ttt.xTurn ? 'x' : 'o')
 
-        board.xTurn = !board.xTurn
+        ttt.xTurn = !ttt.xTurn
     }
 })
