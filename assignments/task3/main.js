212853a80e7948ae06e438d11645060fd6d1d0f7
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-09-15 14:39:15 -0600

    task3 complete

--- assignments/task3/main.js
+++ assignments/task3/main.js
@@ -1,33 +1,42 @@
-function quiz(){
-	let score = 0;
-	let num;
-	let question;
+function quiz() {
+  let score = 0;
+  // let num; -- is this meant to be a red herring? am I missing something???
+  let question;
   let answer;
-	let quiz_questions = [
-	  'How many moons does Earth have?',
-	  'How many moons does Saturn have?',
-	  'How many moons does Venus have?'
-	;
-	let quiz_answers = [1, 82, 0];
-	
-	//get total number of questions
-	let totalQuestion = quiz_questions.count;
-	
-	//generate random number for question
-	let = Math.floor(Math.random() * 3);
-		
-	for(count = 0 count <= totalQuestion; count++)
-		question = quiz_questions[num];
-		answer = prompt(question);
-		if (answer === quiz_answers[num]) {
-			score+;
-			alert("Correct!");
-		} else {
-			alert("Wrong");
-		}
-		num++;
-		if (num = totalQuestion) {
-			num = 0;
+  let quiz_questions = [
+    "How many moons does Earth have?",
+    "How many moons does Saturn have?",
+    "How many moons does Venus have?",
+  ];
+  let quiz_answers = [1, 82, 0];
 
-  document.write('<p>You got ${score} out of ${totalQuestion} questions correct.</p>');
+  //get total number of questions
+  let totalQuestion = quiz_questions.length;
+
+  //generate random number for question -- full disclosure, I have no idea why this is here so I cut it.
+  // num = Math.floor(Math.random() * 3);
+
+  for (let count = 0; count < totalQuestion; count++) {
+    // console.log(count);
+    question = quiz_questions[count];
+    // console.log(question);
+    answer = prompt(question);
+
+    if (answer == quiz_answers[count]) {
+      score++;
+      alert("Correct!");
+    } else {
+      alert("Wrong");
+    }
+
+    // Still don't know what this is supposed to be doing
+    // num++;
+    // if (num === totalQuestion) {
+    //   num = 0;
+    // }
+  }
+
+  let endText = `You got ${score} out of ${totalQuestion} questions correct.`;
+  // console.log(endText);
+  document.getElementById("input").textContent = endText;
 }
