dfd51a63acfe1e8dbc9b018d8aeead563e07363c
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-09-17 08:33:16 -0600

    update form

--- assignments/task2/main.js
+++ assignments/task2/main.js
@@ -1,4 +1,26 @@
 const form = document.querySelector("form");
+const submit = document.querySelector(".submitted");
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let date = document.getElementById("hangout").value;
+  let location = document.getElementById("location").value;
+  let comment = document.getElementById("comment").value;
+
+  //   console.log(name);
+  //   console.log(email);
+  //   console.log(date);
+  //   console.log(location);
+  //   console.log(comment);
+  let submitText = `Sounds good! ${name}, I'll see you in ${location} on ${date} for ${comment}`;
+  form.reset();
+
+  document.querySelector(".submitted").classList.add("visible");
+  document.querySelector(".submit-text").textContent = submitText;
+});
+
+document.querySelector(".close").addEventListener("click", () => {
+  document.querySelector(".submitted").classList.remove("visible");
 });
