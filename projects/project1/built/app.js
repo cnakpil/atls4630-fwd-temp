a32ff92f24e2ab4b382f4c99963d674bf9a35d3d
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-10-11 17:40:14 -0600

    create app.js

---
+++ projects/project1/built/app.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { firebaseConfig } from "./config.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+function App() {
+
+}
+
+export default App;
