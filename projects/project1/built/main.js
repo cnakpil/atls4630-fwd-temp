903368213e8b89239ce727b2273ba5cbdee44a3e
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-10-11 15:00:53 -0600

    update script

--- projects/project1/built/main.js
+++ projects/project1/built/main.js
@@ -1,3 +1,18 @@
 var fillStatus;
 fillStatus = document.querySelector(".wrapper").getAttribute("fill-status");
 console.log(fillStatus);
+// To-do item class to contain information in each list item
+var Todo = /** @class */ (function () {
+    function Todo(text, checked, date) {
+        this.text = text;
+        this.checked = checked;
+        this.id = date;
+    }
+    return Todo;
+}());
+var todoItems;
+function addTodo(input) {
+    todoItems.push(new Todo(input, false, Date.now()));
+    // todoItems.push(newTodo);
+    console.log(todoItems);
+}
--- projects/project1/src/main.ts
+++ projects/project1/src/main.ts
@@ -1,3 +1,28 @@
 let fillStatus: string;
 fillStatus = document.querySelector(".wrapper").getAttribute("fill-status");
 console.log(fillStatus);
+
+// To-do item class to contain information in each list item
+class Todo {
+    text: string;
+    checked: boolean;
+    id: number;
+
+    constructor(text: string, checked: boolean, date: number) {
+        this.text = text;
+        this.checked = checked;
+        this.id = date;
+    }
+}
+
+let todoItems: Array<Todo>;
+
+function addTodo(input) {
+    todoItems.push(new Todo(
+        input,
+        false,
+        Date.now(),
+    ));
+    // todoItems.push(newTodo);
+    console.log(todoItems);
+}
