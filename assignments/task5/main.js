a662182b005a4645fe38bbabd5c68c712e401cf9
cnakpil <85041294+cnakpil@users.noreply.github.com>
2022-11-08 08:53:01 -0700

    update task5

---
+++ assignments/task5/main.js
@@ -0,0 +1,31 @@
+// import fullpage from "https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.js";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+AOS.init();
+
+
+
+
+
+
+
+
+// // var https = require('https');
+
+// const options = {
+//     path: '/json/',
+//     host: 'ipapi.co',
+//     port: 443,
+//     headers: { 'User-Agent': 'nodejs-ipapi-v1.02' }
+// };
+// https.get(options, function (resp) {
+//     var body = ''
+//     resp.on('data', function (data) {
+//         body += data;
+//     });
+
+//     resp.on('end', function () {
+//         var loc = JSON.parse(body);
+//         console.log(loc);
+//     });
+// });
